e4857cd368a6857eb832080d1e83cac1842e82c8
moazam <14061746+mmoazam@users.noreply.github.com>
2024-08-21 12:54:07 +0100

    add h2 access. add exceptions for h2-console. add csrf

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,16 +23,23 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated());
+
+
         http.sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         // http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.httpBasic(withDefaults()).
+                headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin())) // Allow H2 to be accessed in frames
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"));  // Disable CSRF for H2 console
+
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
         UserDetails user1 = User.withUsername("user1")
                 .password("{noop}password1")
                 .roles("USER")
@@ -43,4 +52,8 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(user1, admin);
     }
+
+
 }
+
+
